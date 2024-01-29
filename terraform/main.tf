38bcc2dac510937ecfbd7b6b033b60cc54df2e62
Your Name <you@example.com>
2024-01-29 22:36:52 +0800

    install redit

---
+++ terraform/main.tf
@@ -0,0 +1,62 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  token     = var.token   
+  cloud_id = var.cloud_id
+  folder_id = var.folder_id  
+  zone = var.zone
+}
+
+resource "yandex_compute_instance" "app" {
+  name = "reddit-app"
+  # allow_stopping_for_update = true
+
+  resources {
+    cores  = 2
+    memory = 1
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      # reddit-base-1705684683
+      image_id = var.image_id
+    }
+  }
+
+  network_interface {
+    # default-ru-central1-a
+    subnet_id = var.subnet_id
+    nat = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
+  }
+  
+  connection {
+    type = "ssh"
+    host = yandex_compute_instance.app.network_interface.0.nat_ip_address
+    user = "ubuntu"
+    agent = false
+    # путь до приватного ключа
+    private_key = file("~/.ssh/appuser")
+  }  
+
+  provisioner "file" {
+    source = "files/puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "files/deploy.sh"
+  }
+
+}
