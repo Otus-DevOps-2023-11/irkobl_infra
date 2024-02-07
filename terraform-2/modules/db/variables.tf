051698687b26f90dc62f3f0f68176102b9fd66c5
Your Name <you@example.com>
2024-02-07 22:54:56 +0800

    terraform-2

---
+++ terraform-2/modules/db/variables.tf
@@ -0,0 +1,10 @@
+variable "public_key_path" {
+  description = "Path key"
+}
+variable "private_key" {
+  description = "Privat key"
+}
+variable "db_disk_image" {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
+}
